3312f1fc6138c8d473f32528f51d775a47275daf
Ted Wood <webmaster@thedigitalorchard.ca>
2014-01-22 14:36:22 -0800

    completed the Juggling Async exercise - the difficult seems to have been a variable-scope issue where the queue was not being populated at the proper index due to lack of scope

--- exercises/juggling_async/myprogram.js
+++ exercises/juggling_async/myprogram.js
@@ -1,40 +1,61 @@
 var http = require('http');
+var bl   = require('bl');
 
-var queue = { };
+var queue = [];
 var ended = 0;
 var total = process.argv.length - 2;
 
-var responder = function(response) {
+/**
+*	Wrap http.get() in a function so that we have a scoped index value
+*/
+var http_get = function(idx, url) {
+	http.get(url, function(response) {
 
-	response.setEncoding('utf8');
+		response.pipe(
+			bl(function(err, data) {
+				if (err)
+					return console.error(data);
+				
+				queue[idx] = data.toString();
+				ended++;
 
-	response.on('data', function(str) {
-		this.data += str;
+				if (ended == total) {
+					for (var i = 0; i < total; i++) {
+						console.log(queue[i]);
+					}
+				}
+			})
+		);
 	});
-	
-	response.on('end', function() {
-		ended++;
-		if (ended == total) {
-			output();
-		}
-	});
-};
-
-var output = function() {
-	for (var url in queue) {
-		console.log(queue[url].data);
-	}
 };
 
 for (var i = 0; i < total; i++) {
 
 	var url = process.argv[2+i];
 	
-	queue[url] = { url : url, data : '', responder : responder };
-
+	http_get(i, url);
+	
+	/* -- second attempt using buffer-list
 	http.get(url, function(response) {
-		
-		console.log("HOST: "+response.host);
+
+		response.pipe(
+			bl(function(err, data) {
+				if (err)
+					return console.error(data);
+				
+				queue[i] = data.toString();
+				ended++;
+
+				if (ended == total) {
+					for (var x = 0; x < total; x++) {
+						console.log(queue[x]);
+					}
+				}
+			});
+		);
+
+		//-- first attempt, which does not work correctly
+		response.setEncoding('utf8');
 
 		response.on('data', function(str) {
 			data += str;
@@ -43,8 +64,9 @@ for (var i = 0; i < total; i++) {
 		response.on('end', function() {
 			ended++;
 			if (ended == total) {
-				output();
+				
 			}
 		});
 	});
+	*/
 }
